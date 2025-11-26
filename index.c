fc8db8a4b62ec38541f14cc6598a3b10f46fa775
DPR206 <duna.puente@estudiante.uam.es>
2025-11-26 17:20:35 +0100

    Funciones index y array actualizadas

--- index.c
+++ index.c
@@ -27,38 +27,76 @@ Indexbook *create_Indexbook(int key, long int offset, size_t size)
   return new_index;
 }
 
-int search_register(Indexbook *array, Indexbook index_search, int beginning, int end)
+int bin_search_find(Indexbook **array, Indexbook *index_search, int beginning, int end)
 {
   int pos;
   int middle;
 
-  if (!array || end < beginning || sizeof(array) == 0)
+  if (!array || end < beginning || sizeof(array) == 0 || !index_search || !*array)
   {
-    return -1;
+    return ERROR;
   }
 
-  while (beginning <  end)
+  while (beginning < end)
   {
-    middle = (beginning-end)/2;
+    middle = (beginning - end) / 2;
 
-    if (array[middle].key == index_search.key)
+    if (array[middle]->key == index_search->key)
+    {
+      return middle;
+    }
+    else if (array[middle]->key > index_search->key)
+    {
+      end = middle - 1;
+    }
+    else if (array[middle]->key < index_search->key)
+    {
+      beginning = middle + 1;
+    }
+  }
+
+  return ERROR;
+}
+
+int bin_search_delete(Indexbook **array, Indexbook *index_search, int beginning, int end)
+{
+  int pos;
+  int middle;
+  int end_aux = end;
+
+  if (!array || !*array || !index_search || beginning>end || sizeof(array) == 0 )
+  {
+    return ERROR;
+  }
+  
+  while (beginning < end)
+  {
+    middle = (beginning - end) / 2;
+
+    if (array[middle-1]->key < index_search->key && array[middle]->key > index_search->key)
     {
       return middle;
     } 
-    else if (array[middle].key > index_search.key)
+    else if (array[middle]->key < index_search && array[middle+1]->key > index_search->key)
     {
-      end = middle -1;
-    } 
-    else if (array[middle].key < index_search.key)
-    {
-      beginning = middle+1;
+      return middle++;
     }
-    
-    
+    else if (array[middle]->key > index_search->key && array[middle-1]->key > index_search->key)
+    {
+      end = middle - 1;
+    }
+    else if (array[middle]->key < index_search->key && array[middle+1]->key < index_search)
+    {
+      beginning = middle + 1;
+    }
+  }
+
+  if (array[end_aux]->key < index_search->key)
+  {
+    return end_aux++;
   }
   
-  return -1;
-
+  return ERROR;
 }
 
 void free_Indexbook(Indexbook *indexbook)
@@ -69,77 +107,102 @@ void free_Indexbook(Indexbook *indexbook)
 
 void initArray(Array_index *ai, size_t initalSize)
 {
-  if (!ai || initalSize<0)
+  int i;
+
+  if (!ai || initalSize < 0)
   {
     return;
   }
-  
+
   /*Create initial empty array of size initialSize*/
-  ai->index_array = (Indexbook *)calloc(initalSize, sizeof(Indexbook));
+  ai->index_array = (Indexbook **)calloc(initalSize, sizeof(Indexbook *));
   if (!ai->index_array)
   {
     return;
   }
-  
+
+  for (i = 0; i < initalSize; i++)
+  {
+    ai->index_array[i] = (Indexbook *)calloc(1, sizeof(Indexbook *));
+
+    if (!ai->index_array[i])
+    {
+      freeArray(ai);
+      return;
+    }
+  }
+
   ai->used = 0;
   ai->size = initalSize;
 
   return;
 }
 
-void insertArray(Array_index *ai, Indexbook index)
+Status insertArray(Array_index *ai, Indexbook *index)
 {
   int i;
+  int pos;
 
-  if (!ai || index.key<0)
+  if (!ai || index->key < 0)
   {
-    return;
+    return ERROR;
   }
-  
-  for ( i = 0; i < ai->used; i++)
+
+  for (i = 0; i < ai->used; i++)
   {
-    if (ai->index_array[i].key == index.key)
+    if (ai->index_array[i]->key == index->key)
     {
-      return; /*There cannot be duplicates*/
+      return ERROR; /*There cannot be duplicates*/
     }
-    
   }
 
   /*Insert item index in array*/
-  
+
   if (ai->used == ai->size)
   {
     /*ai->size is the number of entries*/
     ai->size *= 2;
     ai->index_array = realloc(ai->index_array, ai->size * sizeof(Indexbook));
   }
-  
-  /*ai->used is the number of useds entries*/
-  ai->index_array[ai->used++] = index;
 
-  return;
+  pos = bin_search_delete(ai, index, 0, ai->used);
+
+  if (pos == ERROR)
+  {
+    return ERROR;
+  }
+
+  memmove(ai->index_array+pos, ai->index_array+pos, ai->used-pos);
+  ai->index_array[pos] = index;
+  ai->used++;
+
+  /*ai->used is the number of useds entries*/
+  /*ai->index_array[ai->used++] = index;*/
+
+  return OK;
 }
 
-void deletetArray(Array_index *ai, Indexbook index)
+Status deletetArray(Array_index *ai, Indexbook *index)
 {
   int pos;
 
-  if (!ai || index.key<0)
+  if (!ai || index->key < 0)
   {
-    return;
+    return ERROR;
   }
-  
-  pos = search_register(ai->index_array, index, 0, ai->used);
+
+  pos = bin_search_find(ai->index_array, index, 0, ai->used);
   if (pos == -1)
   {
-    return;
+    return ERROR;
   }
-   
+
   free_Indexbook(&ai->index_array[pos]);
-      
-  memmove(ai->index_array+pos, ai->index_array+pos+1, ai->used-pos);
-    
-  return;
+
+  memmove(ai->index_array + pos, ai->index_array + pos + 1, ai->used - pos);
+  ai->used--;
+
+  return OK;
 }
 
 
@@ -147,11 +210,11 @@ void freeArray(Array_index *ai)
 {
   int i;
 
-  for (i = 0; i < ai->used; i++)
+  for (i = 0; i < ai->size; i++)
   {
-    free_Indexbook(&ai->index_array[i]);
+    free_Indexbook(ai->index_array[i]);
   }
-  
+
   /*Free allocated memory for the array*/
   free(ai->index_array);
   ai->index_array = NULL;
